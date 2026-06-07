319c08fc5489a7a615618138b2a3e16e010a1527
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 09:31:46 +0400

    Display the original student name

--- src/main/java/com/example/D_Java/UpdateDemo.java
+++ src/main/java/com/example/D_Java/UpdateDemo.java
@@ -1,12 +1,11 @@
 package com.example.D_Java;
 
 public class UpdateDemo {
-    public static void main(String[] args)
-
-    String CurrentStudentName = "Mawadda";
-    String NewStudentName = "Jehad";
-    boolean isUpdateSuccessful = false;
-    String UpdateStateMassage = " ";
-
+    public static void main(String[] args){
+        String CurrentStudentName = "Mawadda";
+        String NewStudentName = "Jehad";
+        boolean isUpdateSuccessful = false;
+        String UpdateStateMassage = " ";
+    }
 
 }
