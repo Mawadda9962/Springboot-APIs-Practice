45c4aa0b600b1e29fc4b2af853d984adc3664b09
Mawadda <moodalshukaili99@gmail.com>
2026-06-07 20:34:54 +0400

    Adding HashMap

--- src/main/java/com/example/D_Java/Services/RegistrationService.java
+++ src/main/java/com/example/D_Java/Services/RegistrationService.java
@@ -1,4 +1,16 @@
 package com.example.D_Java.Services;
 
+import com.example.D_Java.Entities.EventRegistration;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class RegistrationService {
+
+    private Map<Integer, EventRegistration> registrations = new HashMap<>();
+
+
+
 }
