189621d6d95f85b330bd1dacd354035775d47afd
Mawadda <moodalshukaili99@gmail.com>
2026-06-07 20:31:37 +0400

    Adding Constructor, Getter and Setter

--- src/main/java/com/example/D_Java/Entities/EventRegistration.java
+++ src/main/java/com/example/D_Java/Entities/EventRegistration.java
@@ -5,5 +5,8 @@ public class EventRegistration {
     private int registrationId;
     private String attendeeName;
 
-
+    public EventRegistration(int registrationId, String attendeeName) {
+        this.registrationId = registrationId;
+        this.attendeeName = attendeeName;
+    }
 }
