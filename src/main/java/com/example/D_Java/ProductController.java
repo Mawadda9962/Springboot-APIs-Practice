5d526c907607da3bd3f591d00e5235a193c41469
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 12:36:57 +0400

    Creating a simple in-memory storage

--- src/main/java/com/example/D_Java/ProductController.java
+++ src/main/java/com/example/D_Java/ProductController.java
@@ -1,5 +1,8 @@
 package com.example.D_Java;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -8,6 +11,19 @@ import java.util.HashMap;
 public class ProductController {
 
     HashMap<Integer,Product> productHashMap = new HashMap<>();
+
+    public ProductController(){
+        productHashMap.put(1, new Product(1, "SmartWatch", 10));
+    }
+
+    @PutMapping("/updateStock/{id}")
+    public String updateProductStock(@PathVariable Integer id, @RequestParam int Quantity){
+
+      if (!productHashMap.containsKey(id)){
+          return "Product not found";
+    }
+
+      Product product = productHashMap.get(id);
 
 
 
