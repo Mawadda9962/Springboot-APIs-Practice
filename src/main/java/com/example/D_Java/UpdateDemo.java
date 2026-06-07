4591af2cecd3289c2df83f70dfce650be7af2312
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 09:43:23 +0400

    Compare the current student name and the new student name

--- src/main/java/com/example/D_Java/UpdateDemo.java
+++ src/main/java/com/example/D_Java/UpdateDemo.java
@@ -7,10 +7,18 @@ public class UpdateDemo {
         boolean isUpdateSuccessful = false;
         String UpdateStateMassage = " ";
 
+        //3
         System.out.println("Current Student Name in System: " + CurrentStudentName);
         System.out.println("New Student Name: " + NewStudentName);
 
-        if ()
+        //4
+        System.out.println(CurrentStudentName.equals(NewStudentName));
+
+        if (!CurrentStudentName.equals(NewStudentName)){
+            System.out.println("Names are Different");
+
+
+        }
     }
 
 }
