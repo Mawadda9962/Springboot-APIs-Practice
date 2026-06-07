e2fe29f6cde8631101ad41b34a40bd27bc2d9318
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 11:36:55 +0400

    Compare the current email address and the new email address

--- src/main/java/com/example/D_Java/UpdateEmailDemo.java
+++ src/main/java/com/example/D_Java/UpdateEmailDemo.java
@@ -1,8 +1,17 @@
 package com.example.D_Java;
 
 public class UpdateEmailDemo {
+    public static void main(String[] args){
+        String CurrentEmailAddress = "Mawadda@gmail.com";
+        String NewEmailAddress = "Mawadda.alshukaili@gmail.com";
+        boolean isUpdated = false;
 
-    String CurrentEmailAddress = "Mawadda@gmail.com";
-    String NewEmailAddress = "Mawadda.alshukaili@gmail.com";
-    boolean isUpdated = false;
+
+        System.out.println("Original Email Address: " + CurrentEmailAddress);
+        System.out.println("New Email Address: " + NewEmailAddress);
+
+
+    }
+
+
 }
