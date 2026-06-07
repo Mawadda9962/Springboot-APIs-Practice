a9071d50f53cdc1c47c591088faa15b7caf21fc8
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 15:56:00 +0400

    Declaring the addMember method

--- src/main/java/com/example/D_Java/LibraryDemo.java
+++ src/main/java/com/example/D_Java/LibraryDemo.java
@@ -2,11 +2,23 @@ package com.example.D_Java;
 
 public class LibraryDemo {
     public static void main(String[] args){
+
         LibraryManager libraryManager = new LibraryManager();
-        libraryManager.initializeMembers();
 
+        System.out.println("Before Deletion :");
+        libraryManager.displayMembers();
+        System.out.println(); // Adds a clean empty line
 
+        Integer memberId = 102;
 
+        if (libraryManager.deleteMemberById(memberId)) {
+            System.out.println("Member " + memberId + " deleted successfully.");
+        } else {
+            System.out.println("Member not found.");
+        }
+        System.out.println();
 
+        System.out.println("After Deletion:");
+        libraryManager.displayMembers();
     }
 }
