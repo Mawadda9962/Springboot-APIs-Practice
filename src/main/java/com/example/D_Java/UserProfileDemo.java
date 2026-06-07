d154da51bb8370d5030a887a4fedee06548c3581
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 12:15:01 +0400

    Building a Java Spring Boot UPDATE Endpoint

--- src/main/java/com/example/D_Java/UserProfileDemo.java
+++ src/main/java/com/example/D_Java/UserProfileDemo.java
@@ -5,6 +5,7 @@ public class UserProfileDemo {
 
         UserProfile user = new UserProfile(1001, "92965487", "Muscat");
 
+        System.out.println("********** User Profile Information ***********");
         System.out.println("User Id: " + user.getUserId());
         System.out.println("User phone Number: " + user.getPhoneNumber());
         System.out.println("User Address: " + user.getAddress());
@@ -12,6 +13,12 @@ public class UserProfileDemo {
 
         user.setPhoneNumber("965577743");
         user.setAddress("Sohar");
+
+
+        System.out.println("********** Updated User Profile **********");
+        System.out.println("User Id: " + user.getUserId());
+        System.out.println("User phone Number: " + user.getPhoneNumber());
+        System.out.println("User Address: " + user.getAddress());
     }
 }
 
