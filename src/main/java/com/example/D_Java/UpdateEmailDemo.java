20271d03586e1b0964c7f466039a69d48320a994
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 11:34:53 +0400

    Assigning Values to Variables

--- src/main/java/com/example/D_Java/UpdateEmailDemo.java
+++ src/main/java/com/example/D_Java/UpdateEmailDemo.java
@@ -2,6 +2,7 @@ package com.example.D_Java;
 
 public class UpdateEmailDemo {
 
-    String CurrentEmailAddress;
-    String
+    String CurrentEmailAddress = "Mawadda@gmail.com";
+    String NewEmailAddress = "Mawadda.alshukaili@gmail.com";
+    boolean isUpdated = false;
 }
