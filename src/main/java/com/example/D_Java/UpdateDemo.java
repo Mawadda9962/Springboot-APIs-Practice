7e74a5448a6b2d2811a1d819fc0c5aad844f5fb7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 09:48:00 +0400

    Display the final value of the student name

--- src/main/java/com/example/D_Java/UpdateDemo.java
+++ src/main/java/com/example/D_Java/UpdateDemo.java
@@ -24,6 +24,7 @@ public class UpdateDemo {
             System.out.println("The existing value already matches the new value");
         }
 
+        System.out.println("Final Student Name: " + CurrentStudentName);
 
     }
 
