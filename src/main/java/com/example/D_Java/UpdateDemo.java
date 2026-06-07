bba7d6faa0a99dae8e341a44f7d01c86cb1fb693
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 09:31:20 +0400

    Declaring Variables

--- src/main/java/com/example/D_Java/UpdateDemo.java
+++ src/main/java/com/example/D_Java/UpdateDemo.java
@@ -1,6 +1,7 @@
 package com.example.D_Java;
 
 public class UpdateDemo {
+    public static void main(String[] args)
 
     String CurrentStudentName = "Mawadda";
     String NewStudentName = "Jehad";
