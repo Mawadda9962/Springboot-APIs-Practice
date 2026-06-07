d07df679c2c76d596c74725154454d3f25094881
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 15:49:37 +0400

    Declaring the addMember method

---
+++ src/main/java/com/example/D_Java/LibraryDemo.java
@@ -0,0 +1,12 @@
+package com.example.D_Java;
+
+public class LibraryDemo {
+    public static void main(String[] args){
+        LibraryManager libraryManager = new LibraryManager();
+        libraryManager.initializeMembers();
+
+
+
+
+    }
+}
--- src/main/java/com/example/D_Java/LibraryManager.java
+++ src/main/java/com/example/D_Java/LibraryManager.java
@@ -11,7 +11,7 @@ public class LibraryManager {
     }
 
 
-    private void initializeMembers() {
+    public void initializeMembers() {
         addMember(new LibraryMember(101, "Ahmed", "Standard"));
         addMember(new LibraryMember(102, "Sara", "Premium"));
         addMember(new LibraryMember(103, "John", "Standard"));
@@ -19,5 +19,31 @@ public class LibraryManager {
 
     public void addMember(LibraryMember member){
         this.members.add(member);
+    }
+
+    public void displayMembers(){
+        if (members.isEmpty()){
+            System.out.println("There is no members");
+        }else {
+            for (LibraryMember member : members){
+                System.out.println("ID: " + member.getMemberId() + "Name: "
+                + member.getFullName() + "Type: " + member.getMembershipType());
+            }
+        }
+    }
+
+    public boolean deleteMemberById(Integer memberId) {
+        for (int i = 0; i < members.size(); i++) {
+            LibraryMember currentMember = members.get(i);
+
+            if (currentMember.getMemberId().equals(memberId)) {
+                members.remove(i);
+                i--;
+                System.out.println("Member found and deleted");
+                return true;
+            }
+        }
+        System.out.println("Member not found! ");
+        return false;
     }
 }
