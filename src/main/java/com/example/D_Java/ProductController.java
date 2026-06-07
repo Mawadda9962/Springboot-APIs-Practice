d68e05a921101b9b00ed66826edca1999a003c1a
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 12:29:35 +0400

    Creating a simple in-memory storage

--- src/main/java/com/example/D_Java/ProductController.java
+++ src/main/java/com/example/D_Java/ProductController.java
@@ -1,4 +1,14 @@
 package com.example.D_Java;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
 public class ProductController {
+
+    HashMap<Integer,Product> productHashMap = new HashMap<>();
+
+
+
 }
