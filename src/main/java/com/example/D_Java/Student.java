95ad12dbe0d885ba9db4c21788783da8b0dd904b
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 09:56:51 +0400

    Adding private fields

--- src/main/java/com/example/D_Java/Student.java
+++ src/main/java/com/example/D_Java/Student.java
@@ -3,7 +3,14 @@ package com.example.D_Java;
 public class Student {
     private Integer StudentId;
     private String StudentName;
-    private Double Grade;
+    private String Grade;
+
+
+    public Student(Integer studentId, String studentName, String grade) {
+        StudentId = studentId;
+        StudentName = studentName;
+        Grade = grade;
+    }
 
 
 }
