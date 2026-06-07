5e444e2fcda4a58bb202062b611990722b1bca06
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 12:00:46 +0400

    Creating a new object

---
+++ src/main/java/com/example/D_Java/UserProfileDemo.java
@@ -0,0 +1,12 @@
+package com.example.D_Java;
+
+public class UserProfileDemo {
+    public static void main(String[] args){
+
+        UserProfile user = new UserProfile(1001, "92965487", "Muscat");
+
+        System.out.println("User Id: " + user.getUserId());
+        System.out.println();
+
+    }
+}
