a8cf561cb26860a60e3b7337f645a9d179d43695
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 16:05:00 +0400

    Declaring the addMember method

--- src/main/java/com/example/D_Java/productDelete.java
+++ src/main/java/com/example/D_Java/productDelete.java
@@ -10,4 +10,28 @@ public class productDelete {
         this.productName = productName;
         this.price = price;
     }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
