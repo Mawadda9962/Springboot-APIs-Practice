4adf89ccec488d95fe338b127a8ff3bb1c4c8fe7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-07 09:05:19 +0400

    initilize new project setup from spring initializer

---
+++ src/main/java/com/example/D_Java/DJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.D_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/D_Java/DJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.D_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
