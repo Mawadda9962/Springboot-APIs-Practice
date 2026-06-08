3ddfb6be26c3f8dd2573d221c1a537e148cfa9f4
Mawadda <moodalshukaili99@gmail.com>
2026-06-08 23:00:02 +0400

    Creating a new class Vehicle

--- src/main/java/com/example/D_Java/Services/VehicleManager.java
+++ src/main/java/com/example/D_Java/Services/VehicleManager.java
@@ -1,4 +1,12 @@
 package com.example.D_Java.Services;
 
+import com.example.D_Java.Entities.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class VehicleManager {
+    private List<Vehicle> vehicles = new ArrayList<>();
+
+
 }
