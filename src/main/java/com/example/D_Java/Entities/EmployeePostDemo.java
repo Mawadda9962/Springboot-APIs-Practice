175e557dd4c7072076d253ab9f1fd3ccb050c1c9
Mawadda <moodalshukaili99@gmail.com>
2026-06-08 21:43:05 +0400

    Create Employee Collection & Add 3 Sample Employees

--- src/main/java/com/example/D_Java/Entities/EmployeePostDemo.java
+++ src/main/java/com/example/D_Java/Entities/EmployeePostDemo.java
@@ -1,8 +1,16 @@
 package com.example.D_Java.Entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeePostDemo {
     public static void main(String[] args){
 
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("E101", "Sara", "HR"));
+        employeeList.add(new Employee("E102", "Ahmed", "Finance"));
+        employeeList.add(new Employee("E103", "John", "Engineering"));
     }
 
 }
