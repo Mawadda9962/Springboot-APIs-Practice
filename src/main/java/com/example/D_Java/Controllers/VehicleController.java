ff59c0e2e74726a33cf5cdd77976078b94053eae
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-09 09:36:28 +0400

    fix the bug

--- src/main/java/com/example/D_Java/Controllers/VehicleController.java
+++ src/main/java/com/example/D_Java/Controllers/VehicleController.java
@@ -1,6 +1,18 @@
 package com.example.D_Java.Controllers;
 
+import com.example.D_Java.Services.VehicleManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class VehicleController {
+    @Autowired
+    VehicleManager vehicleManager;
+
+    @
+
 
 
 }
