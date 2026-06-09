68db858718993f67e95cdc5b0335b2616268a9cd
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-09 09:39:09 +0400

    fix the bug

--- src/main/java/com/example/D_Java/Controllers/VehicleController.java
+++ src/main/java/com/example/D_Java/Controllers/VehicleController.java
@@ -1,9 +1,11 @@
 package com.example.D_Java.Controllers;
 
+import com.example.D_Java.Entities.Vehicle;
 import com.example.D_Java.Services.VehicleManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -11,7 +13,15 @@ public class VehicleController {
     @Autowired
     VehicleManager vehicleManager;
 
-    @
+    @GetMapping
+    public List<Vehicle> getAllVehicles(){
+        return vehicleManager.getAllVehicles();
+    }
+
+    @PostMapping
+    public String addVehicle(@RequestBody Vehicle vehicle){
+        return vehicleManager.addVehicle(vehicle);
+    }
 
 
 
