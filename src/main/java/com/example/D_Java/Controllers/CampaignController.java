02d9fc48a55b80daaa16cd1d3542f0d7626eb08e
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-09 10:04:09 +0400

    fix the bug

--- src/main/java/com/example/D_Java/Controllers/CampaignController.java
+++ src/main/java/com/example/D_Java/Controllers/CampaignController.java
@@ -1,6 +1,8 @@
 package com.example.D_Java.Controllers;
 
+import com.example.D_Java.Entities.Campaign;
 import com.example.D_Java.Entities.Vehicle;
+import com.example.D_Java.Services.CampaignManager;
 import com.example.D_Java.Services.VehicleManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -8,22 +10,20 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("vehicles")
+@RequestMapping("campaigns")
 public class CampaignController {
 
     @Autowired
-    VehicleManager vehicleManager;
+    CampaignManager campaignManager;
 
     @GetMapping
-    public List<Vehicle> getAllVehicles(){
-        return vehicleManager.getAllVehicles();
+    public List<Campaign> getAllCampaign(){
+        return campaignManager.getAllCampaigns();
     }
 
     @PostMapping
-    public String addVehicle(@RequestBody Vehicle vehicle){
-        return vehicleManager.addVehicle(vehicle);
+    public String addCampaign(@RequestBody Campaign campaign){
+        return campaignManager.addCampaign(campaign);
     }
-
-
 
 }
