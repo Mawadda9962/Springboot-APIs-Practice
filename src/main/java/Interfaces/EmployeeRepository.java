f5cda260597baadf286a31c458e5e818e0a89b58
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-09 11:55:17 +0400

    Database Integration

--- src/main/java/Interfaces/EmployeeRepository.java
+++ src/main/java/Interfaces/EmployeeRepository.java
@@ -1,5 +1,12 @@
 package Interfaces;
 
-public interface EmployeeRepository {
+import com.example.D_Java.Entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,String> {
+
+
 
 }
