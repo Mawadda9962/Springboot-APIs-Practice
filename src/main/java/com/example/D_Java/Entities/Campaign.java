4103dc0378f525f5a379dfdcd994dd3cf65ccf3a
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-09 11:51:08 +0400

    Database Integration

--- src/main/java/com/example/D_Java/Entities/Campaign.java
+++ src/main/java/com/example/D_Java/Entities/Campaign.java
@@ -1,11 +1,13 @@
 package com.example.D_Java.Entities;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Campaign {
         private String campaignId;
         private String campaignName;
--- src/main/java/com/example/D_Java/Entities/Employee.java
+++ src/main/java/com/example/D_Java/Entities/Employee.java
@@ -1,15 +1,27 @@
 package com.example.D_Java.Entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private String employeeId;
     private String employeeName;
     private String department;
+
+    @OneToMany
+    List<Campaign> campaigns;
+    @OneToMany
+    List<Vehicle> vehicles;
 }
